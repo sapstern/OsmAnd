fae6c1429d5a4f79cc88acc4b70640326c28dccf
Corwin-Kh <serge.kharchenko@gmail.com>
2024-11-11 17:47:27 +0200

    Fixes after refactor

--- OsmAnd/src/net/osmand/plus/plugins/odb/VehicleMetricsPlugin.kt
+++ OsmAnd/src/net/osmand/plus/plugins/odb/VehicleMetricsPlugin.kt
@@ -93,7 +93,6 @@ class VehicleMetricsPlugin(app: OsmandApplication) : OsmandPlugin(app), OBDReadS
 	private var connectionStateListener: ConnectionStateListener? = null
 	private var pairingDevice: BTDeviceInfo? = null
 
-	private var socket: BluetoothSocket? = null
 	private var obdDispatcher: OBDDispatcher? = null
 
 	enum class OBDConnectionState {
@@ -323,10 +322,9 @@ class VehicleMetricsPlugin(app: OsmandApplication) : OsmandPlugin(app), OBDReadS
 		return deviceList
 	}
 
+	@MainThread
 	fun disconnect() {
 		obdDispatcher?.stopReading()
-		socket?.safeClose()
-		socket = null
 		val lastConnectedDeviceInfo = connectedDeviceInfo
 		connectedDeviceInfo = null
 		setLastConnectedDevice(null)
@@ -403,6 +401,7 @@ class VehicleMetricsPlugin(app: OsmandApplication) : OsmandPlugin(app), OBDReadS
 		val obdDispatcher = OBDDispatcher(debug)
 		obdDispatcher.setReadStatusListener(this)
 		this.obdDispatcher = obdDispatcher
+		OBDDataComputer.obdDispatcher = obdDispatcher
 		return obdDispatcher
 	}
 
@@ -421,12 +420,16 @@ class VehicleMetricsPlugin(app: OsmandApplication) : OsmandPlugin(app), OBDReadS
 			override fun onConnectionFailed() {
 				handler.post { onDisconnected(deviceToConnect) }
 			}
+
+			override fun disconnect() {
+			}
 		})
 	}
 
 	@SuppressLint("MissingPermission")
 	private fun connectToDevice(activity: Activity, connectedDevice: BluetoothDevice) {
 		createOBDDispatcher().connect(object : OBDConnector {
+			private var socket: BluetoothSocket? = null
 			val deviceToConnect = BTDeviceInfo(
 				connectedDevice.getAliasName(activity),
 				connectedDevice.address)
@@ -447,6 +450,13 @@ class VehicleMetricsPlugin(app: OsmandApplication) : OsmandPlugin(app), OBDReadS
 					LOG.error("Can't connect to device. $error")
 				}
 				return null
+			}
+
+			override fun disconnect() {
+				socket?.apply {
+					safeClose()
+					socket = null
+				}
 			}
 
 			override fun onConnectionSuccess() {
@@ -518,12 +528,9 @@ class VehicleMetricsPlugin(app: OsmandApplication) : OsmandPlugin(app), OBDReadS
 	}
 
 	override fun onIOError() {
-		socket?.apply {
-			safeClose()
-			disconnect()
-			handler.removeCallbacksAndMessages(null)
-			handler.postDelayed({ reconnectObd() }, RECONNECT_DELAY)
-		}
+		handler.post { disconnect() }
+		handler.removeCallbacksAndMessages(null)
+		handler.postDelayed({ reconnectObd() }, RECONNECT_DELAY)
 	}
 
 	private fun reconnectObd() {
@@ -533,10 +540,6 @@ class VehicleMetricsPlugin(app: OsmandApplication) : OsmandPlugin(app), OBDReadS
 				connectToObd(it, lastConnectedDevice)
 			}
 		}
-	}
-
-	override fun onInitConnectionFailed() {
-		disconnect()
 	}
 
 	override fun updateLocation(location: Location) {
@@ -729,7 +732,9 @@ class VehicleMetricsPlugin(app: OsmandApplication) : OsmandPlugin(app), OBDReadS
 		return getWidgetValue(computerWidget, null)
 	}
 
-	fun getWidgetValue(computerWidget: OBDDataComputer.OBDComputerWidget, obdWidgetOptions: OBDWidgetOptions?): String {
+	fun getWidgetValue(
+		computerWidget: OBDDataComputer.OBDComputerWidget,
+		obdWidgetOptions: OBDWidgetOptions?): String {
 		val data = computerWidget.computeValue()
 		if (data == "N/A") {
 			return "N/A"
@@ -750,6 +755,7 @@ class VehicleMetricsPlugin(app: OsmandApplication) : OsmandPlugin(app), OBDReadS
 			OBDDataComputer.OBDTypeWidget.ENGINE_OIL_TEMPERATURE,
 			OBDDataComputer.OBDTypeWidget.TEMPERATURE_AMBIENT,
 <<<<<<< HEAD
+<<<<<<< HEAD
 			OBDDataComputer.OBDTypeWidget.TEMPERATURE_COOLANT -> getConvertedTemperature(data as Number, obdWidgetOptions)
 =======
 			OBDDataComputer.OBDTypeWidget.TEMPERATURE_COOLANT -> getConvertedTemperature(data as Number)
@@ -757,6 +763,13 @@ class VehicleMetricsPlugin(app: OsmandApplication) : OsmandPlugin(app), OBDReadS
 >>>>>>> ca41659c72 (UI fixes; Removed displaying unknown device when searching new obd; added connection to obd when AA connected)
 			OBDDataComputer.OBDTypeWidget.FUEL_LEFT_LITER -> getFormattedVolume(data as Number)
 
+=======
+			OBDDataComputer.OBDTypeWidget.TEMPERATURE_COOLANT -> getConvertedTemperature(
+				data as Number,
+				obdWidgetOptions)
+
+			OBDDataComputer.OBDTypeWidget.FUEL_LEFT_LITER -> getFormattedVolume(data as Number)
+>>>>>>> 653f6cfbec (Fixes after refactor)
 			OBDDataComputer.OBDTypeWidget.FUEL_CONSUMPTION_RATE_LITER_HOUR -> getFormatVolumePerHour(
 				data as Number)
 
@@ -783,7 +796,9 @@ class VehicleMetricsPlugin(app: OsmandApplication) : OsmandPlugin(app), OBDReadS
 		return getWidgetUnit(computerWidget, null)
 	}
 
-	fun getWidgetUnit(computerWidget: OBDDataComputer.OBDComputerWidget, obdWidgetOptions: OBDWidgetOptions?): String? {
+	fun getWidgetUnit(
+		computerWidget: OBDDataComputer.OBDComputerWidget,
+		obdWidgetOptions: OBDWidgetOptions?): String? {
 		return when (computerWidget.type) {
 			OBDDataComputer.OBDTypeWidget.SPEED -> getSpeedUnit()
 			OBDDataComputer.OBDTypeWidget.RPM -> app.getString(R.string.rpm_unit)
@@ -803,7 +818,8 @@ class VehicleMetricsPlugin(app: OsmandApplication) : OsmandPlugin(app), OBDReadS
 			OBDDataComputer.OBDTypeWidget.TEMPERATURE_COOLANT,
 			OBDDataComputer.OBDTypeWidget.TEMPERATURE_INTAKE,
 			OBDDataComputer.OBDTypeWidget.ENGINE_OIL_TEMPERATURE,
-			OBDDataComputer.OBDTypeWidget.TEMPERATURE_AMBIENT -> (obdWidgetOptions?.getTemperatureUnit()?.symbol ?: getTemperatureUnit().symbol)
+			OBDDataComputer.OBDTypeWidget.TEMPERATURE_AMBIENT -> (obdWidgetOptions?.getTemperatureUnit()?.symbol
+				?: getTemperatureUnit().symbol)
 
 			OBDDataComputer.OBDTypeWidget.BATTERY_VOLTAGE -> app.getString(R.string.unit_volt)
 			OBDDataComputer.OBDTypeWidget.FUEL_TYPE,
@@ -821,7 +837,7 @@ class VehicleMetricsPlugin(app: OsmandApplication) : OsmandPlugin(app), OBDReadS
 	}
 
 	private fun getConvertedTemperature(data: Number, obdWidgetOptions: OBDWidgetOptions?): Float {
-		val temperatureUnit = obdWidgetOptions?.getTemperatureUnit()?: getTemperatureUnit()
+		val temperatureUnit = obdWidgetOptions?.getTemperatureUnit() ?: getTemperatureUnit()
 		val temperature = data.toFloat()
 		return if (temperatureUnit == TemperatureUnit.CELSIUS) {
 			temperature
