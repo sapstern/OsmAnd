caca8c2829bf3bd22a0408ef64f56ce749cf89dd
Corwin-Kh <serge.kharchenko@gmail.com>
2024-11-27 11:21:06 +0200

    Fixed crash on recreating obd widgets

--- OsmAnd/src/net/osmand/plus/plugins/odb/OBDTextWidget.kt
+++ OsmAnd/src/net/osmand/plus/plugins/odb/OBDTextWidget.kt
@@ -160,7 +160,6 @@ open class OBDTextWidget(
 
 	override fun updateSimpleWidgetInfo(drawSettings: DrawSettings?) {
 		val visible = widgetType.isPurchased(app)
-		updateVisibility(visible)
 		if (visible) {
 			updateSimpleWidgetInfoImpl()
 		}
